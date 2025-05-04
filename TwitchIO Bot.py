8cf9584a299d981ae6c83048d058fa238473f928
neg-is <negar.sakhipour@gmail.com>
2025-05-04 19:22:42 +0200

    Initial commit with proper .gitignore

--- TwitchIO Bot.py
+++ TwitchIO Bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('TWITCH_TOKEN')  # should look like oauth:xxxxxxx
-CHANNEL = os.getenv('TWITCH_CHANNEL')  # should be like neglovegood
+CHANNEL = os.getenv('TWITCH_CHANNEL')
 
 # Simple bot class
 class Bot(commands.Bot):
